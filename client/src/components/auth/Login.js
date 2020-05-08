011f6e35aca6b7630126309df664507fa5b44c0d
Wesley Melo <wesley_melo@gap.com>
2020-05-08 02:15:57 -0300

    adding resgister and login pages

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const {email, password} = formData
+
+  const onChange = (event) => setFormData({
+    ...formData, [event.target.name]: event.target.value
+  })
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    console.log('SUCCESS')  
+}
+
   return (
-    <div>
-      Login
-    </div>
+    <Fragment>
+        <h1 className="large text-primary">Sign In</h1>
+        <p className="lead"><i className="fas fa-user"></i> Join into your account</p>
+        <form className="form" onSubmit={event => onSubmit(event)}>
+          <div className="form-group">
+            <input type="email" placeholder="Email Address" name="email" value={email} onChange={event => onChange(event)} required />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="8"
+              value={password} onChange={event => onChange(event)}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Do not have an account? <Link to="/register">Sign Up</Link>
+        </p>
+    </Fragment>
   )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {name, email, password, password2} = formData
+
+  const onChange = (event) => setFormData({
+    ...formData, [event.target.name]: event.target.value
+  })
+
+  const onSubmit = async (event) => {
+    event.preventDefault()
+
+    if(password !== password2){
+      console.log('Passwords do not mach')
+    }else{
+      console.log('SUCCESS')
+  }
+}
+
   return (
-    <div>
-      Register
-    </div>
+    <Fragment>
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+        <form className="form" onSubmit={event => onSubmit(event)}>
+          <div className="form-group">
+            <input type="text" placeholder="Name" name="name" value={name} onChange={event => onChange(event)} required />
+          </div>
+          <div className="form-group">
+            <input type="email" placeholder="Email Address" name="email" value={email} onChange={event => onChange(event)} required />
+            <small className="form-text"
+              >This site uses Gravatar so if you want a profile image, use a
+              Gravatar email</small>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="8"
+              value={password} onChange={event => onChange(event)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              minLength="8"
+              value={password2} onChange={event => onChange(event)}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+    </Fragment>
   )
 }
 
